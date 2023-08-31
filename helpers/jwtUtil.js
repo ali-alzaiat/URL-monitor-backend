75928534cd9b102b3b37ccd0610859f78e972cd8
ali-alzaiat <alialzaiat2000@gmail.com>
2023-08-31 21:40:42 +0300

    added verify token

--- helpers/jwtUtil.js
+++ helpers/jwtUtil.js
@@ -5,3 +5,6 @@ exports.generateToken = (userName,email)=>{
     var token = jwt.sign({ userName: userName, email:email }, process.env.SECRET_KEY, {expiresIn:"3h"});
     return token;
 }
+exports.verifyToken = (token)=>{
+    return jwt.verify(token,process.env.SECRET_KEY);
+}
